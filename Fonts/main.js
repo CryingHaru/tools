5ea07a41acbfe70cdef0701f8e35cfd69c5de390
HaruCraft <luisaraniva2@gmail.com>
2023-10-30 17:52:23 -0600

    send

--- Fonts/main.js
+++ Fonts/main.js
@@ -1,48 +1,124 @@
 let fuente = 'Arial';
 const canvas = document.getElementById('imagen');
 const ctx = canvas.getContext('2d');
-const divt = document.getElementById('right');
+const divt = document.getElementById('rightDiv');
 const inputFuentes = document.getElementById("fuentes");
 let inputId = 0;
-
+canvas.width = 800;
+canvas.height = 800;
 function crearInputs() {
-    const inputTexto = document.createElement('input');
-    inputTexto.type = 'text';
-    inputTexto.id = `texto${inputId}`;
+    if (inputId < 11) {
+        const inputTexto = document.createElement('input');
+        inputTexto.type = 'text';
+        inputTexto.id = `texto${inputId}`;
 
-    const inputTamanioFuente = document.createElement('input');
-    inputTamanioFuente.type = 'number';
-    inputTamanioFuente.step = '0.0';
-    inputTamanioFuente.min = '0';
-    inputTamanioFuente.max = '1000';
-    inputTamanioFuente.value = '30';
-    inputTamanioFuente.id = `TamanioFuente${inputId}`;
-  
+        const inputTamanioFuente = document.createElement('input');
+        inputTamanioFuente.type = 'number';
+        inputTamanioFuente.step = '0.0';
+        inputTamanioFuente.min = '0';
+        inputTamanioFuente.max = '1000';
+        inputTamanioFuente.value = '40';
+        inputTamanioFuente.id = `TamanioFuente${inputId}`;
 
-    const input = document.createElement('input');
-    input.type = 'file';
-    input.name = 'Fuentes';
-    input.id = `fuentes${inputId}`;
-    input.accept = '.ttf,.otf,.woff,.woff2,.zip';
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.style.display = 'none';
+        input.name = 'Fuentes';
+        input.id = `fuentes${inputId}`;
+        input.accept = '.ttf,.otf,.woff,.woff2,.zip';
+
+        const label = document.createElement('label');
+        label.htmlFor = `fuentes${inputId}`;
+        label.innerText = `Fuente ${inputId + 1}`;
+
+        inputId++;
+        divt.appendChild(inputTexto);
+        divt.appendChild(inputTamanioFuente);
+        divt.appendChild(input);
+        divt.appendChild(label);
+        divt.appendChild(document.createElement('br'));
+    }
+}
+// Function to load a font from a file
+function loadFont(fontFile) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const font = new FontFace('MyFont', e.target.result);
+            font.load().then(function (loadedFont) {
+                document.fonts.add(loadedFont);
+                resolve('MyFont');
+            }).catch(function (error) {
+                reject(error);
+            });
+        };
+        reader.readAsArrayBuffer(fontFile);
+    });
+}
+// Function to render text with a specific font
+function renderText(input, TamanioFuente, font, y) {
+    ctx.font = `${TamanioFuente}px ${font}`;
+    const text = input.value;
+    const textMetrics = ctx.measureText(text);
+    const textHeight = textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent;
     
-    inputId++;
-    divt.appendChild(inputTexto);
-    divt.appendChild(inputTamanioFuente);
-    divt.appendChild(input);
+    // Adjust y position based on font size
+    const adjustedY = y + Number(TamanioFuente);
+    
+    ctx.fillText(text, canvas.width / 2, adjustedY);
+    
+    console.log(adjustedY);
+    console.log(TamanioFuente);
+    console.log(text);
+    console.log(textHeight);
+    return adjustedY + textHeight;
 }
 
-function llenarTexto() {
+async function llenarTexto() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.textAlign = 'center';;
-    let y = 50; 
+    ctx.textAlign = 'center';
+    let y = 50; // Inicializa la posición vertical.
+
     const inputs = document.querySelectorAll('input[type="text"]');
-    console.log(inputs);
     const fuentes = document.querySelectorAll('input[type="number"]');
+    const fontFiles = document.querySelectorAll('input[type="file"]');
+
+    const fonts = [];
+
+    for (let i = 0; i < fontFiles.length; i++) {
+        const TamanioFuente = fuentes[i].value;
+        const input = inputs[i];
+        const fontFile = fontFiles[i].files[0];
+
+        try {
+            const font = await loadFont(fontFile);
+            fonts.push(font);
+        } catch (error) {
+            console.log('Error loading font: ' + error);
+            fonts.push('Arial'); // Usar Arial como respaldo en caso de error.
+        }
+    }
+
     for (let i = 0; i < inputs.length; i++) {
         const TamanioFuente = fuentes[i].value;
         const input = inputs[i];
-        ctx.font = TamanioFuente + 'px ' + fuente + ' Black';
-        ctx.fillText(input.value, canvas.width / 2, y);
-        y += Number(TamanioFuente) + 10; 
+        const font = fonts[i]; // Obtener la fuente correspondiente.
+
+        // Dibuja el texto y obtén su métrica de altura.
+       
+        y = renderText(input, TamanioFuente, font, y);
     }
 }
+
+
+
+function downloadcanvas() {
+    // Convert the canvas to a Blob object
+    const dataUrl = canvas.toDataURL('image/png');
+    const a = document.createElement('a');
+    a.href = dataUrl;
+    a.download = 'canvas.png';
+    a.click();
+};
+
+
